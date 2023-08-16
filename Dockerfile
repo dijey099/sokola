d27cf97d9ba2019a05187bdfad4ab6638814ee0f
dijey099 <lionnelrazaf@gmail.com>
2023-08-16 12:35:46 +0300

    changing app to python with CRUD

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
-FROM httpd:2.4
+FROM python:3.4-alpine
 
-COPY . /usr/local/apache2/htdocs/
+ADD . /sokola_app
+WORKDIR /sokola_app
+RUN pip install -r requirements.txt
+
+CMD ["gunicorn", "server:app", "-b", "0.0.0.0:80"]
---
+++ server.py
@@ -0,0 +1,103 @@
+from flask import Flask, render_template, request, make_response
+import requests
+
+app = Flask(__name__, template_folder='.', static_folder='./static')
+
+db_url = 'http://192.168.1.106:4001'
+
+
+def execute_query(query):
+    url = f'{db_url}/db/query?associative&q={query}'
+    
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+def write_query(query_list):
+    url = f'{db_url}/db/execute'
+    
+    response = requests.post(url, json=query_list)
+    r = response.json()
+    
+    if response.status_code == 200 and 'rows_affected' in r['results'][0]:
+        return r
+    else:
+        return None
+
+
+@app.route('/', methods=['GET'])
+def web():
+    results = execute_query("SELECT * FROM sokola")
+    rows = results['results'][0]['rows']
+    name_pairs = []
+
+    # Iterate over the rows in pairs
+    for i in range(0, len(rows), 2):
+        if i + 1 < len(rows):
+            pair = (rows[i]['NAME'], rows[i + 1]['NAME'])
+        else:
+            pair = (rows[i]['NAME'], "")
+        name_pairs.append(pair)
+
+    print(name_pairs)
+    
+    return render_template('index.html', sokola_names=name_pairs)
+
+
+@app.route('/admin', methods=['GET'])
+def admin():
+    results = execute_query("SELECT * FROM sokola")
+    names = []
+    if results:
+        names = [(item['ID'], item['NAME']) for item in results['results'][0]['rows']]
+
+    return render_template('admin.html', message="", sokola_names=names)
+    # return render_template('admin.html', message="")
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    name = request.form['sokola-name']
+    
+    query = f"INSERT INTO sokola (NAME) VALUES ('{name}')"
+    write_query([query])
+
+    results = execute_query("SELECT * FROM sokola")
+    names = []
+    if results:
+        names = [(item['ID'], item['NAME']) for item in results['results'][0]['rows']]
+    
+    return render_template('admin.html', message="Tafiditra", sokola_names=names)
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    ID = request.args['id']
+    
+    query = f"DELETE FROM sokola WHERE ID={ID}"
+    r = write_query([query])
+    if r == None:
+        return make_response({'code': 400}, 400)
+    
+    return make_response({'code': 200}, 200)
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    ID = request.args['id']
+    newName = request.args['name']
+    
+    query = f"UPDATE sokola SET NAME='{newName}' WHERE ID={ID}"
+    r = write_query([query])
+    if r == None:
+        return make_response({'code': 400}, 400)
+    
+    return make_response({'code': 200}, 200)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
